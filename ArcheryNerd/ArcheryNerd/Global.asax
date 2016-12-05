2380ce038928769e1b2be4e04d6633c59fcef75b
Allen Crane <Geekstarexe@Gmail.com>
2016-12-05 12:28:47 -0600

    Just the first commit nothing really special

---
+++ ArcheryNerd/ArcheryNerd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArcheryNerd.MvcApplication" Language="C#" %>
---
+++ ArcheryNerd/ArcheryNerd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArcheryNerd.Startup))]
+namespace ArcheryNerd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
